46bc0680ae788862482155bfae558d0295fb4a5a
Игорь Голяк <golyak.03@inbox.ru>
2021-05-15 10:38:25 +0700

    12

---
+++ 12.05.2021/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._05._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3.
+            Console.WriteLine("Введите число p");
+            double p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число r");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число m");
+            double m = Convert.ToDouble(Console.ReadLine());
+            double m1 = p * m;
+            Console.WriteLine(m1);
+            Console.ReadKey();
+
+
+
+            
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //13.
+            Console.WriteLine("Введите число p1");
+            int p1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число c");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число p2");
+            int p2 = Convert.ToInt32(Console.ReadLine());
+            int a1 = p1 - p2;
+            int c1 = a + b;
+            int d = c + c1;
+            Console.WriteLine(c1);
+            Console.WriteLine(d);
+            Console.WriteLine(a1);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
